e172748961fcff2eb2879fae9e3b94c7c070b059
hyltonjean <hyltonjean@gmail.com>
2020-04-07 22:16:42 +0200

    Initial commit with auth and laravel/socialite integration.

---
+++ app/Http/Controllers/SocialsController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class SocialsController extends Controller
+{
+  public function redirect($provider)
+  {
+    return Socialite::driver($provider)->redirect();
+  }
+
+  public function Callback($provider)
+  {
+    $userSocial =   Socialite::driver($provider)->stateless()->user();
+    $users       =   User::where(['email' => $userSocial->getEmail()])->first();
+
+    if ($users) {
+      Auth::login($users);
+      return redirect('home');
+    } else {
+      $user = User::create([
+        'name'          => $userSocial->getName(),
+        'email'         => $userSocial->getEmail(),
+        'image'         => $userSocial->getAvatar(),
+        'provider_id'   => $userSocial->getId(),
+        'provider'      => $provider,
+      ]);
+      return redirect()->route('home');
+    }
+  }
+}
